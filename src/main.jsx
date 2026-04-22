099d352e60a68ed92ab3648d3ac8aab46c0487a7
Suriya <ktaro112@gmail.com>
2026-04-22 10:26:34 +0700

    chore: setup folder structure and Tailwind design tokens

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'  // ← แก้ path ตรงนี้
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
